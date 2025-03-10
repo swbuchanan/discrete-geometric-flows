b63898d7b3a2c0bbe46ef2e347be02d83669d9aa
Steven <stevenman42@gmail.com>
2025-03-10 22:30:38 +1100

    aoeu

--- flow.ts
+++ flow.ts
@@ -100,6 +100,12 @@ class FlowCurve {
         this.normals = [];
     }
 
+    // remove a vertex and its normal vector
+    removeVertex(idx: number) {
+        this.vertices.splice(idx,1);
+        this.normals.splice(idx,1);
+    }
+
     // add a point at the position of the cursor
     addVertex(event: MouseEvent, canvas: HTMLCanvasElement) {
         const rect = canvas.getBoundingClientRect();
@@ -149,16 +155,38 @@ class FlowCurve {
         bisector = differenceToLeft.add(differenceToRight).normalize();
 
         // I think the .5 is just to make the speed nice
-        bisector = bisector.scale(.5*kappa);
+        bisector = bisector.scale(0.5*kappa);
         return bisector;
 
 
         return new Vector(0,0); // TODO
     }
 
+    calculateNormals() {
+        for (let i = 0; i < this.normals.length; i++) {
+            this.calculateNormal(i);
+        }
+    }
+
     reparametrize() {
         // this is complicated
+        // the idea is to redistribute the vertices along the curve in a good way
         
+        // first idea: if any two adjacent vertices are too far apart (say distance 10), they get a new vertex added at the midpoint between them
+        
+        for (let i = 0; i < this.vertices.length; i++) {
+            const nextIdx = (i+1) % this.vertices.length;
+            if (this.vertices[i].distanceTo(this.vertices[nextIdx]) > 100) {
+                console.log(`vertex ${i} and vertex ${nextIdx} are too darn far apart`);
+                const x = (this.vertices[i].x + this.vertices[nextIdx].x)/2
+                const y = (this.vertices[i].y + this.vertices[nextIdx].y)/2;
+                console.log(`vertex ${i} is at position (${this.vertices[i].x}, ${this.vertices[i].y}), and vertex ${i+1} is at position (${this.vertices[nextIdx].x}, ${this.vertices[nextIdx].y}), so I'm adding a vertex at (${x}, ${y})`);
+                this.vertices.splice(i+1, 0, new Vector(x,y));
+                this.normals.splice(i+1, 0, new Vector(0,0));
+            }
+        }
+        
+        this.calculateNormals();
     }
 
     draw(canvas: HTMLCanvasElement) {
@@ -196,11 +224,21 @@ class FlowCurve {
             this.clearVertices();
             return;
         }
+
+        this.reparametrize();
         
         // first move all the points
         for (let i = 0; i < this.vertices.length; i++) {
             this.vertices[i].x = this.vertices[i].x + this.normals[i].x;
             this.vertices[i].y = this.vertices[i].y + this.normals[i].y;
+        }
+
+        // remove any adjacent points that are now on top of each other
+        for (let i = 0; i < this.vertices.length; i++) {
+            if (this.vertices[i].distanceTo(this.vertices[(i+1) % this.vertices.length]) < 1) {
+                console.log(`removing vertex ${i}`);
+                this.removeVertex(i);
+            }
         }
 
         // recalculate the normal vectors of all the moved points
